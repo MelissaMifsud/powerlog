a6977653f7553d7e09215ccca18437ff3810da7a
Melissa <melissa.anne.mifsud@gmail.com>
2015-03-09 22:37:33 +1000

    #1 - Adding hashing function structure

---
+++ src/main/java/net/melissam/powerlog/hashing/ShiftAndXorHashingFunction.java
@@ -0,0 +1,10 @@
+package net.melissam.powerlog.hashing;
+
+public class ShiftAndXorHashingFunction implements StringHashingFunction{
+
+	public int hash(String s) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/main/java/net/melissam/powerlog/hashing/StringHashingFunction.java
@@ -0,0 +1,13 @@
+package net.melissam.powerlog.hashing;
+
+/**
+ * Hashing function implementation. 
+ * 
+ * @author melissam
+ *
+ */
+public interface StringHashingFunction {
+
+	int hash(String s);
+	
+}
