257c80bdf48f1f77a14a60916c34828ac3e4f99f
Melissa <melissa.anne.mifsud@gmail.com>
2015-05-29 23:49:27 +1000

    Added packaging with dependencies

--- clustream-remote-clustering/src/main/resources/localdb.sql
+++ clustream-remote-clustering/src/main/resources/localdb.sql
@@ -12,7 +12,7 @@ CREATE TABLE Feature
 	id 					INTEGER 		NOT NULL, -- the feature id (corresponds to the line number in the dataset)
 	time				INTEGER			NOT NULL, -- time at which the feature was received
 	feature 			VARCHAR(1024) 	NOT NULL, -- the serialized feature
-	groundTruth			VARCHAR(20),			  -- the ground truth classification of the feature, if available		
+	label				VARCHAR(20),			  -- the ground truth classification of the feature, if available		
 	clusterId			INTEGER 		NOT NULL, -- the id of the cluster the feature was initially placed into
 	
 	CONSTRAINT feature_pk PRIMARY KEY (id)
@@ -22,10 +22,12 @@ CREATE TABLE Feature
 -- Table that stores the status of the clusters at intervals of the clustering
 CREATE TABLE Snapshot
 (
-	time			INTEGER 		NOT NULL,  -- time at which the status is being recorded
-	featureCount	INTEGER			NOT NULL,  -- number of features received so far
-	lastFeature		INTEGER			NOT NULL,  -- id of the last feature that was clustered
-	clusters		CLOB			NOT NULL,  -- serialized representation of cluster ids and id-lists
+	time 				INTEGER 		NOT NULL, 	-- time at which the status is being recorded
+	clusterId			INTEGER			NOT NULL, 	-- id of cluster
+	idList				VARCHAR(1024),				-- list of clusters that were merged with this cluster	
+	sumOfValues			VARCHAR(1024)	NOT NULL,	-- serialised array of sum of attributes
+	sumSquareOfValues	VARCHAR(1024)	NOT NULL,	-- serialised array of sum of squares of values
+	size				INTEGER			NOT NULL,	-- number of features in the cluster
 	
-	CONSTRAINT snapshot_pk PRIMARY KEY (timestamp)
+	CONSTRAINT snapshot_pk PRIMARY KEY (time, clusterId)
 );
